8ffb2af42f2b0f8a6803f5aeb3b3f2f1073ab0a6
BeatingHeartMS <49695336+BeatingHeartMS@users.noreply.github.com>
2024-08-27 15:55:52 -0300

    Update app.py 2708

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
 from gtts import gTTS
 import os
 
 app = Flask(__name__)
 
-# Usar el directorio temporal de Render para almacenar el archivo de audio
 AUDIO_DIR = '/tmp'
+last_command = None
 
 @app.route('/text-to-speech', methods=['POST'])
 def text_to_speech():
+    global last_command
     data = request.get_json()
     text = data.get('text', '')
     if text:
         tts = gTTS(text=text, lang='es', tld='com.ar')
         file_path = os.path.join(AUDIO_DIR, 'audio.mp3')
         tts.save(file_path)
-        return jsonify({'message': 'Audio file created successfully', 'audio_url': '/audio.mp3'})
+        last_command = text  # Almacenar el último comando
+        return jsonify({'message': 'Audio file created successfully', 'audio_url': f'/audio.mp3'})
     return jsonify({'error': 'No text provided'}), 400
 
-
-@app.route('/audio.mp3', methods=['GET'])
-def get_audio():
-    # Servir el archivo de audio desde el directorio temporal
-    return send_from_directory(AUDIO_DIR, 'audio.mp3')
+@app.route('/get-latest-command', methods=['GET'])
+def get_latest_command():
+    global last_command
+    if last_command:
+        command = last_command
+        last_command = None  # Reiniciar después de enviar
+        return jsonify({'text': command})
+    return jsonify({'text': ''})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
